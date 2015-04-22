9d7a13192270e2f0b283362492dab84e7da1625c
SmallEvil97 <SmallEvil97@gmail.com>
2015-04-22 18:39:08 +0300

    Update analysis_text.py

--- analysis_text.py
+++ analysis_text.py
@@ -1,8 +1,8 @@
 import string
-words = {}
-for line in open('my_text'):
-    for word in line.lower().split():
-        word = word.strip(string.punctuation)
-        words[word] = words.get(word, 0) + 1
-for word in sorted(words):
-    print("'{0}'".format(word),"found:",words[word])
+words = {} # создаю словарь  
+for line in open('my_text'): # даю путь к файлу
+    for word in line.lower().split(): # пройдемся по всем элементам в файле  
+        word = word.strip(string.punctuation) # исключаю пунктуацию из слов
+        words[word] = words.get(word, 0) + 1 # каждый раз когда встречаються одинаковые ключи (или новые), происходит + 1 
+for word in sorted(words): # сортируем словарь 
+    print("'{0}'".format(word),"found:",words[word]) # вывожу принт
